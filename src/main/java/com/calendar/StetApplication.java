a49a7c205952d3d3a77052d6f94f28c1561d6c93
Golovanov <stanislav.golovanov@simbirsoft.com>
2021-01-29 17:59:21 +0300

    Initial commit

---
+++ src/main/java/com/calendar/StetApplication.java
@@ -0,0 +1,13 @@
+package com.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StetApplication.class, args);
+	}
+
+}
